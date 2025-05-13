990eae7148a4123d95267bd9050d5d52db398cef
Mathujan2000 <ps202893@summacollege.nl>
2025-05-13 09:42:41 +0200

    Fixed Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,16 +10,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
     public function test_home_redirects_to_habits()
     {
         $response = $this->get('/');
+
         $response->assertRedirect('/habits');
     }
 
